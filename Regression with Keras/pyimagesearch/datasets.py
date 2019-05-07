152ee6ca439a4affdaa381e0a3f58663dd58600a
Mrinal <waliamrinal@gmail.com>
2019-05-07 15:59:20 +0530

    added the images input datasset

--- Regression with Keras/pyimagesearch/datasets.py
+++ Regression with Keras/pyimagesearch/datasets.py
@@ -54,3 +54,56 @@ def process_house_attributes(df, train, test):
  
 	# return the concatenated training and testing data
 	return (trainX, testX)
+
+def load_house_images(df, inputPath):
+	# initialize our images array (i.e., the house images themselves)
+	images = []
+ 
+	# loop over the indexes of the houses
+	for i in df.index.values:
+		# find the four images for the house and sort the file paths,
+		# ensuring the four are always in the *same order*
+		basePath = os.path.sep.join([inputPath, "{}_*".format(i + 1)])
+		housePaths = sorted(list(glob.glob(basePath)))
+		gression, and CNNsPython
+		# initialize our list of input images along with the output image
+		# after *combining* the four input images
+		inputImages = []
+		outputImage = np.zeros((64, 64, 3), dtype="uint8")
+
+		# loop over the input house paths
+		for housePath in housePaths:
+			# load the input image, resize it to be 32 32, and then
+			# update the list of input images
+			image = cv2.imread(housePath)
+			image = cv2.resize(image, (32, 32))
+			inputImages.append(image)
+
+		# initialize our list of input images along with the output image
+		# after *combining* the four input images
+		inputImages = []
+		outputImage = np.zeros((64, 64, 3), dtype="uint8")
+ 
+		# loop over the input house paths
+		for housePath in housePaths:
+			# load the input image, resize it to be 32 32, and then
+			# update the list of input images
+			image = cv2.imread(housePath)
+			image = cv2.resize(image, (32, 32))
+			inputImages.append(image)
+			
+			# tile the four input images in the output image such the first
+		# image goes in the top-right corner, the second image in the
+		# top-left corner, the third image in the bottom-right corner,
+		# and the final image in the bottom-left corner
+		outputImage[0:32, 0:32] = inputImages[0]
+		outputImage[0:32, 32:64] = inputImages[1]
+		outputImage[32:64, 32:64] = inputImages[2]
+		outputImage[32:64, 0:32] = inputImages[3]
+ 
+		# add the tiled image to our set of images the network will be
+		# trained on
+		images.append(outputImage)
+ 
+	# return our set of images
+	return np.array(images)
