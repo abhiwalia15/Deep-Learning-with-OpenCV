addfcd07368b019deeda8438c3350eea0c502099
Mrinal <waliamrinal@gmail.com>
2019-05-07 16:13:01 +0530

    datasets updated

--- Regression with Keras/pyimagesearch/models.py
+++ Regression with Keras/pyimagesearch/models.py
@@ -22,3 +22,25 @@ def create_mlp(dim, regress=False):
 
 	# return our model
 	return model
+	
+def create_cnn(width, height, depth, filters=(16, 32, 64), regress=False):
+	# initialize the input shape and channel dimension, assuming
+	# TensorFlow/channels-last ordering
+	inputShape = (height, width, depth)
+	chanDim = -1
+	
+	# define the model input
+	inputs = Input(shape=inputShape)
+ 
+	# loop over the number of filters
+	for (i, f) in enumerate(filters):
+		# if this is the first CONV layer then set the input
+		# appropriately
+		if i == 0:
+			x = inputs
+ 
+		# CONV => RELU => BN => POOL
+		x = Conv2D(f, (3, 3), padding="same")(x)
+		x = Activation("relu")(x)
+		x = BatchNormalization(axis=chanDim)(x)
+		x = MaxPooling2D(pool_size=(2, 2))(x)
